5f8aaf1425ea419b717f1a5cfdd70966d0558637
RichardAlvin <richardalvinpratama8@gmail.com>
2022-04-08 12:24:39 +0700

    add authentication

--- market.py
+++ market.py
@@ -4,19 +4,3 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# from flask import Flask, render_template
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# @app.route('/home')
-# def home_page():
-#     return render_template('home.html')
-
-
-# @app.route('/market')
-# def market_page():
-#     return render_template('market.html', item_name='Phone')
--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,37 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("auth/login.html", boolean=True)
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in Successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.market'))
+            else:
+                flash('Incorrect pass!', category='error')
+        else:
+            flash('Email doesnt exist', category='error')
+
+    return render_template("auth/login.html", user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/signup', methods=['GET', 'POST'])
@@ -22,7 +43,10 @@ def sign_up():
         password = request.form.get('password')
         cpassword = request.form.get('cpassword')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 4 characters.', category='error')
         elif len(fname) < 2:
             flash('First name must be greater than 2 characters.', category='error')
@@ -31,7 +55,11 @@ def sign_up():
         elif len(password) < 7:
             flash('Password must be greater than 7 characters.', category='error')
         else:
+            new_user = User(email=email, first_name=fname, last_name=lname,
+                            password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account Created!', category='success')
-            # add user to database
+            return redirect(url_for('views.market'))
 
     return render_template("auth/sign_up.html")
